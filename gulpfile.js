499493cca34705e24b3b2c28da7606af58fc4a57
Esteban Fuentealba <efuentealba@json.cl>
2015-09-13 12:01:12 -0300

    Eliminados archivos que ya no se usan

--- gulpfile.js
+++ gulpfile.js
@@ -7,12 +7,6 @@ var browserify  = require('gulp-browserify');
 var uglify      = require('gulp-uglify');
 
 gulp.task("build-js", function () {
-  gulp.src("./src/app.js")
-    .pipe(browserify({
-      transform: ['babelify']
-    }))
-    //.pipe(uglify())
-    .pipe(gulp.dest("./build/"));
   gulp.src("./src/background.js")
     .pipe(browserify({
       transform: ['babelify']
@@ -22,7 +16,7 @@ gulp.task("build-js", function () {
 });
 gulp.task("cp", function() {
   //gulp.src(["./src/background.js","./src/icon.png","./src/manifest.json","./src/popup.html"])
-  gulp.src(["./src/icon-19x19.png","./src/manifest.json","./src/popup.html","./src/index.html","./src/css/default.css"])
+  gulp.src(["./src/icon-19x19.png","./src/manifest.json"])
     .pipe(gulp.dest('./build/'));
 });
 gulp.task('default', [ 'cp', 'build-js' ]);
